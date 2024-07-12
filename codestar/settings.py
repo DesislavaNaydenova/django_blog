5f8984ed68e6f991ccba8d7d811d93d3ee85aeff
Desislava Naydenova <deskata.01@gmail.com>
2024-07-12 06:56:07 +0000

    Build the Post model nd add it to the DB

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-desislavanay-djangoblog-v16qkcqchdw.ws-eu115.gitpod.io','.herokuapp.com']
 
@@ -132,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#List of trusted origins for request
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*herokuapp"
+]
