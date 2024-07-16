f367faf9fc201a19d54cc4b7b9d8bf8e51870636
Desislava Naydenova <deskata.01@gmail.com>
2024-07-16 10:40:34 +0000

    Perform some debugging

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     status= models.IntegerField(choices=STATUS, default=0)
     excerpt= models.TextField(blank=True, null=True)
     updated_on= models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["created_on"]
     def __str__(self):
@@ -23,6 +24,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on= models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
