6a6e7ea830930854915259db9b92ece615524239
Desislava Naydenova <deskata.01@gmail.com>
2024-07-22 10:50:40 +0000

    Add About and Post models in model.py in te about app, register the models in admin.py

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, About
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content', 'excerpt')
+
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'created_on', 'updated_on')
+    summernote_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(About, AboutAdmin)
