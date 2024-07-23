6f31c41e574ea009e2520ccb5ea0883749e292a7
Desislava Naydenova <deskata.01@gmail.com>
2024-07-23 12:44:07 +0000

    Add a comment count and filter to the views and added a for-loop to post-detail.html to show only the approved comments

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on= models.DateTimeField(auto_now_add=True)
+    challenge = models.TextField(blank=True, null=True)
     
     class Meta:
         ordering = ["-created_on"]
