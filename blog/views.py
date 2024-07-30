735502256157a6e21d0517804ed8da7a47ad3061
Desislava Naydenova <deskata.01@gmail.com>
2024-07-30 08:42:29 +0000

    Delete print statemnts from views.py in blog

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,6 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved = True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -35,7 +34,6 @@ def post_detail(request, slug):
              'Comment submitted and awaiting approval!')
 
     comment_form = CommentForm()
-    print("About to render template")
     return render(request, "blog/post_detail.html", {"post": post,
     "comments": comments,
     "comment_count": comment_count,
