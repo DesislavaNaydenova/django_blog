9a8af975b776b8b60f005e5b14c55e2e6bb2ac94
Desislava Naydenova <deskata.01@gmail.com>
2024-07-30 07:07:19 +0000

    Add tests for the collaboration form

--- about/test_forms.py
+++ about/test_forms.py
@@ -14,3 +14,29 @@ class TestCollaborateForm(TestCase):
         })
         self.assertTrue(form.is_valid(), msg="Form is not valid")
 
+    def test_name_is_required(self):
+        """ Test for name field"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(form.is_valid(), msg="Name was not provided, but the form is valid")
+
+    def test_email_is_required(self):
+        """ Testfor email field """
+        form = CollaborateForm({
+            'name': 'test',
+            'email': '',
+            'message': 'Hello'
+        })
+        self.assertFalse(form.is_valid(), msg="Email was not provided, but the form is valid")
+
+    def test_message_is_required(self):
+        """ Test for message field """
+        form = CollaborateForm({
+            'name': 'test',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid(), msg="Message was not provided, but the form is valid")
