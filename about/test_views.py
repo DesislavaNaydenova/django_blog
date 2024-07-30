09b36b8bf7c32a6396355e6e3a820c08f788bbeb
Desislava Naydenova <deskata.01@gmail.com>
2024-07-30 09:48:49 +0000

    Create code for tastingthe submittion of the collaboration form

--- about/test_views.py
+++ about/test_views.py
@@ -17,3 +17,12 @@ class TestAboutViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'About Me', response.content)
         self.assertIsInstance(response.context['collaborate_form'], CollaborateForm)
+
+    def test_successful_submition_collaboration_form(self):
+        post_data= {
+            'name': 'User',
+            'email': 'test@test.com',
+            'message': 'Iwould like to work with you'}
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Collaboration request received! I endeavour to respond within 2 working days.', response.content)
